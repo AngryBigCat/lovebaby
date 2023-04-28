b9f058b2cccc7f21e35f17b5764cf8338fddc5e6
AngryCat <angrycat123@163.com>
2023-04-28 11:10:54 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'https://api.telegram.org/bot6241720862:AAHo1nUnhNaBipEDGZM3_WnpslFGYpKk4jw/sendMessage' \
+--header 'User-Agent: Apifox/1.0.0 (https://www.apifox.cn)' \
+--data-urlencode 'chat_id=-969622166' \
+--data-urlencode 'text=love you ~'
