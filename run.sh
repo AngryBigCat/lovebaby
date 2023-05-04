fec86f04af7fe3c798b90b7d463face89bb0fea8
AngryCat <angrycat123@163.com>
2023-05-04 14:52:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
-text=$(curl "https://api.vvhan.com/api/love?type=json"|jq -r ".ishan")
+TEXT=$(curl "https://api.vvhan.com/api/love?type=json")
 
-echo $text
+echo $TEXT
+
+MSG=$(echo $TEXT|jq -r ".ishan")
+
+echo $MSG
 
 curl --location --request POST 'https://api.telegram.org/bot6241720862:AAHo1nUnhNaBipEDGZM3_WnpslFGYpKk4jw/sendMessage' \
 --header 'User-Agent: Apifox/1.0.0 (https://www.apifox.cn)' \
 --data-urlencode 'chat_id=-969622166' \
---data-urlencode "text=$text"
+--data-urlencode "text=$MSG"
